2a82eafd2509b1d49f4002e94324a911319c385e
Eom, Dasom <deom3@gatech.edu>
2018-06-19 13:07:35 -0400

    Created new activity for the detailed textview

---
+++ MARTA/app/src/main/java/com/example/cs2340/marta/list.java
@@ -0,0 +1,13 @@
+package com.example.cs2340.marta;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class list extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list);
+    }
+}
