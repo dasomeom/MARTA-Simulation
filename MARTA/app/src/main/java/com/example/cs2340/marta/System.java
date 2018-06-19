5ec483387d1e137a826c04eefc82df174e32f72d
Eom, Dasom <deom3@gatech.edu>
2018-06-19 19:37:39 -0400

    deleted unused import line

--- MARTA/app/src/main/java/com/example/cs2340/marta/System.java
+++ MARTA/app/src/main/java/com/example/cs2340/marta/System.java
@@ -5,17 +5,11 @@ package com.example.cs2340.marta;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-<<<<<<< HEAD
-import android.view.View;
-import android.widget.Button;
-=======
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
-import android.widget.Toast;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
